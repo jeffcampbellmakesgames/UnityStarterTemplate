3fc70097b32afb83628ca2af9b382e885dc6844e
Jeff Campbell <jeff@zapdot.com>
2022-12-09 09:57:05 -0500

    Fixed bug for unavailable console commands
    * Fixed bug for unavailable console commands by modifying them to be static. Refactored them to ConsoleCommands.

--- Unity/Assets/Game/Scripts/Architecture/ConsoleCommands.cs
+++ Unity/Assets/Game/Scripts/Architecture/ConsoleCommands.cs
@@ -8,6 +8,22 @@ namespace Game
 	/// </summary>
 	public class ConsoleCommands : MonoBehaviour
 	{
+		/// <summary>
+		/// Hides the main menu and loads the game scene.
+		/// </summary>
+		[ConsoleMethod("game-enter", "Enters the game from the main menu.")]
+		public static void EnterGameCommand()
+		{
+			GameControl.Instance.EnterGame();
+		}
 
+		/// <summary>
+		/// Unloads the game scene and shows the main menu.
+		/// </summary>
+		[ConsoleMethod("game-exit", "Exits the game back to the main menu.")]
+		public static void ExitGameCommand()
+		{
+			GameControl.Instance.ExitGame();
+		}
 	}
 }
